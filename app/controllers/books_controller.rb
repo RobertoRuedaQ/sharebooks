2c217bdabbc540a2b0ad771c59fc8b9cd3506bf6
cmg <desarrollo@cmg.com.co>
2018-01-18 15:40:13 -0500

    refine the view for books and log in/sign in

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,7 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
+    @sidebar = true
     @books = Book.search(params[:term])
   end
 
@@ -83,6 +84,6 @@ class BooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
-      params.require(:book).permit(:title, :author, :pages, :image, :abstract, :user_id)
+      params.require(:book).permit(:title, :author, :pages, :image, :abstract, :user_id, :category_id)
     end
 end
